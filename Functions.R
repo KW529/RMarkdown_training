7b98c448583ed2e53082546245472d8fe851eee8
Kate Wilkins <kate.wilkins@colostate.edu>
2020-11-17 09:31:48 -0800

    Verifying function

--- Functions.R
+++ Functions.R
@@ -1,15 +1,23 @@
 #Practicing Functions in R
+
 airtemps<-c(212,30.3,78,32)
 airtemps
+
+#Farenheit to Celsius
 fahr_to_celsius <- function(fahr) {
   celsius <- (fahr-32)*5/9
   return(celsius)
-} #From opening curly brace to the end 
-# is the functioning body 
+} #From opening curly brace to the end is the functioning body 
 
+#Celsius to Fahrenheit
 celsius <- fahr_to_celsius(airtemps)
 
 c_to_f<-function(cel){fahr<-(cel*(9/5))+32
         return(fahr)}
 
 fahr.new<-c_to_f(celsius)
+
+celsius==fahr_to_celsius(airtemps)
+
+
+
