ec871a4ca526cfb0f1e7ed4a926d07d00a7241c1
Kate Wilkins <kate.wilkins@colostate.edu>
2020-11-17 09:25:58 -0800

    Practicing functions in R

---
+++ Functions.R
@@ -0,0 +1,15 @@
+#Practicing Functions in R
+airtemps<-c(212,30.3,78,32)
+airtemps
+fahr_to_celsius <- function(fahr) {
+  celsius <- (fahr-32)*5/9
+  return(celsius)
+} #From opening curly brace to the end 
+# is the functioning body 
+
+celsius <- fahr_to_celsius(airtemps)
+
+c_to_f<-function(cel){fahr<-(cel*(9/5))+32
+        return(fahr)}
+
+fahr.new<-c_to_f(celsius)
